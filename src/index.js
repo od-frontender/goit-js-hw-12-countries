8861fc2de98654cd9b8ed63c3fe59b947ca7b10a
od-frontender <od.frontender@gmail.com>
2021-08-13 20:03:21 +0400

    поправил баг

--- src/index.js
+++ src/index.js
@@ -20,18 +20,31 @@ const debounce = require('lodash.debounce');
 
 refs.inputCountry.addEventListener('input', debounce(searchCountry, 500));
 
+function searchCountry(e) {
+  refs.countryContainer.innerHTML = '';
+  fetchCountries(e.target.value)
+    .then(countries => {
+      if (countries.status === 404) {
+        noticeIncorrect();
+        return;
+      }
+      markupCountryCard(countries);
+    })
+    .catch(() => {
+      refs.countryContainer.innerHTML = '';
+      noticeIncorrect();
+    });
+}
 function markupCountryCard(countries) {
   if (countries.length > 10) {
     noticeError();
     return;
   }
-
   if (countries.length >= 2 && countries.length <= 10) {
-    renderCountriesList(countries);
+    markupCountriesList(countries);
     noticeProgress();
     return;
   }
-
   renderCountryCard(countries);
   noticeSuccess();
 }
@@ -39,26 +52,7 @@ function renderCountryCard(countries) {
   const countryCardMarkUp = countryCardTemplate(countries);
   refs.countryContainer.insertAdjacentHTML('beforeend', countryCardMarkUp);
 }
-function renderCountriesList(countries) {
+function markupCountriesList(countries) {
   const countriesList = countriesListTemplate(countries);
   refs.countryContainer.insertAdjacentHTML('beforeend', countriesList);
-}
-
-function searchCountry(e) {
-  refs.countryContainer.innerHTML = '';
-  const searchQuery = e.target.value;
-
-  fetchCountries(searchQuery)
-    .then(countries => {
-      if (countries.status === 404) {
-        noticeIncorrect();
-        return;
-      }
-
-      markupCountryCard(countries);
-    })
-    .catch(() => {
-      refs.countryContainer.innerHTML = '';
-      noticeIncorrect();
-    });
 }
